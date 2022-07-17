714ebe23ed6ef60843e4094b52ff981c47a9a262
dezhizhang <1541609448@qq.com>
2022-07-17 12:03:17 +0800

    feat:update

--- handler/brand.go
+++ handler/brand.go
@@ -2,14 +2,20 @@ package handler
 
 import (
 	"context"
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"sales-product-srv/driver"
 	"sales-product-srv/model"
 	"sales-product-srv/proto"
 	"sales-product-srv/utils"
+	"time"
 )
 
 type BrandServer struct {
 }
+
+// 获取品牌列表
 
 func (b *BrandServer) GetBrandList(ctx context.Context, req *proto.BrandRequest) (*proto.BrandResponseList, error) {
 	var count int64
@@ -37,4 +43,32 @@ func (b *BrandServer) GetBrandList(ctx context.Context, req *proto.BrandRequest)
 	rsp.Total = int32(count)
 	rsp.Data = brandsList
 	return rsp, nil
+}
+
+// 创建品牌
+
+func (b *BrandServer) CreateBrand(ctx context.Context, req *proto.CreateBrandRequest) (*empty.Empty, error) {
+	var brand model.Brand
+	if result := driver.DB.Where("name=?", req.Name).Find(brand); result.RowsAffected == 1 {
+		return nil, status.Errorf(codes.InvalidArgument, "品牌已存在")
+	}
+	brand.Id = req.Id
+	brand.Name = req.Name
+	brand.Logo = req.Logo
+	brand.DeletedAt = time.Now()
+	err := driver.DB.Save(&brand).Error
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "创建品牌失败")
+	}
+	return &empty.Empty{}, nil
+}
+
+// 删除品牌
+
+func (c *BrandServer) DeleteBrand(ctx context.Context, req *proto.DeleteBrandRequest) (*empty.Empty, error) {
+	var brand model.Brand
+	if result := driver.DB.Where("id = ?", req.Id).Delete(&brand); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.Internal, "删除失败")
+	}
+	return &empty.Empty{}, nil
 }
--- test/main.go
+++ test/main.go
@@ -1,21 +1,40 @@
 package main
 
-//func main() {
-//	var err error
-//	conn, err := grpc.Dial("127.0.0.1:8086", grpc.WithInsecure())
-//	if err != nil {
-//		fmt.Println("连接失败")
-//		return
-//	}
-//	brandSrvClient := proto.NewBrandClient(conn)
-//	rsp, err := brandSrvClient.GetBrandList(context.Background(), &proto.BrandRequest{
-//		PageIndex: 1,
-//		PageSize:  10,
-//	})
-//
-//	if err != nil {
-//		fmt.Printf("查询失败%s", err.Error())
-//		return
-//	}
-//
-//}
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"sales-product-srv/proto"
+)
+
+func main() {
+	var err error
+	conn, err := grpc.Dial("127.0.0.1:8086", grpc.WithInsecure())
+	if err != nil {
+		fmt.Println("连接失败")
+		return
+	}
+	brandSrvClient := proto.NewBrandClient(conn)
+	//for i := 0; i < 1000; i++ {
+	//	rsp, err := brandSrvClient.CreateBrand(context.Background(), &proto.CreateBrandRequest{
+	//		Id:   fmt.Sprintf("change%d", i),
+	//		Name: fmt.Sprintf("小米%d", i),
+	//		Logo: fmt.Sprintf("https://www.baidu.com"),
+	//	})
+	//	if err != nil {
+	//		fmt.Println(err.Error())
+	//	}
+	//	fmt.Println(rsp)
+	//}
+	rsp, err := brandSrvClient.GetBrandList(context.Background(), &proto.BrandRequest{
+		PageIndex: 1,
+		PageSize:  10,
+	})
+
+	if err != nil {
+		fmt.Printf("查询失败%s", err.Error())
+		return
+	}
+	fmt.Println(rsp)
+
+}
