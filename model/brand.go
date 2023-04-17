801d7373a45d55aaccba39c3922601642edc26b1
zhangdezhi <zhangdezhi@tungee.com>
2023-04-17 22:24:11 +0800

    feat:修改表结构

--- model/brand.go
+++ model/brand.go
@@ -3,13 +3,13 @@ package model
 type Brand struct {
 	BaseModel
 	Name string `gorm:"type:varchar(32);not null" json:"name"`
-	Logo string `gorm:"type:varchar(200);not null" json:"logo"`
+	Logo string `gorm:"type:varchar(200);default:'';not null" json:"logo"`
 }
 
 type ProductCategoryBrand struct {
 	BaseModel
 	Category   Category `json:"category"`
-	CategoryId int32    `gorm:"type:int;index:idx:category_brand,unique" json:"categoryId"`
+	CategoryId int32    `gorm:"type:int;index:idx_category_brand,unique" json:"categoryId"`
 	Brand      Brand    `json:"brand"`
-	BrandId    int32    `gorm:"type:int;idx:category_brand,unique" json:"brandId"`
+	BrandId    int32    `gorm:"type:int;index:idx_category_brand,unique" json:"brandId"`
 }
--- model/category.go
+++ model/category.go
@@ -5,6 +5,6 @@ type Category struct {
 	Name             string    `gorm:"type:varchar(64);not null" json:"name"`
 	ParentCategoryId int32     `json:"parentCategoryId"`
 	ParentCategory   *Category `json:"productCategory"`
-	Level         ss   int32     `gorm:"type:int;not null;default:1" json:"level"`
+	Level            int32     `gorm:"type:int;not null;default:1" json:"level"`
 	IsTab            bool      `gorm:"type:bool;default:false;not null" json:"isTab"`
 }
