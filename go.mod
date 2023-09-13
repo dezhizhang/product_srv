fd7ef6a5aa1846d29855c313f2f86de130ab9f55
dezhizhang <1541609448@qq.com>
2023-09-13 23:19:20 +0800

    feat:商品服务

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/bwmarrin/snowflake v0.3.0
+	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/golang/protobuf v1.5.3
 	google.golang.org/grpc v1.28.0-pre
 	google.golang.org/protobuf v1.30.0
@@ -12,13 +13,14 @@ require (
 )
 
 require (
-	github.com/go-redis/redis v6.15.9+incompatible // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
-	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	golang.org/x/net v0.4.0 // indirect
-	golang.org/x/sys v0.3.0 // indirect
-	golang.org/x/text v0.5.0 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.27.10 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
 	google.golang.org/genproto v0.0.0-20200115191322-ca5a22157cba // indirect
 )
--- main.go
+++ main.go
@@ -1,21 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"google.golang.org/grpc"
 	"net"
-	"os"
-	"os/signal"
 	"sales-product-srv/handler"
 	"sales-product-srv/proto"
-	"syscall"
 )
 
 func main() {
 	server := grpc.NewServer()
-	proto.RegisterCategoryServer(server, &handler.CategoryServer{})
-	//proto.RegisterBannerServer(server, &handler.BannerServer{})
-	proto.RegisterBrandServer(server, &handler.BrandServer{})
+	proto.RegisterProductServer(server, &handler.ProductServer{})
+	//proto.RegisterCategoryServer(server, &handler.CategoryServer{})
+	////proto.RegisterBannerServer(server, &handler.BannerServer{})
+	//proto.RegisterBrandServer(server, &handler.BrandServer{})
 	listen, err := net.Listen("tcp", "127.0.0.1:8086")
 	if err != nil {
 		panic("failed to listen" + err.Error())
@@ -25,20 +22,8 @@ func main() {
 	//if err != nil {
 	//	panic("failed to start grpc" + err.Error())
 	//}
-	go func() {
-		err = server.Serve(listen)
-		if err != nil {
-			panic("failed to start grpc:" + err.Error())
-		}
-	}()
-
-	//接收终止信号
-	quit := make(chan os.Signal)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-	fmt.Println("服务注册成功")
-	//if err = client.Agent().ServiceDeregister(serviceID); err != nil {
-	//	zap.S().Info("注销失败")
-	//}
-	//zap.S().Info("注销成功")
+	err = server.Serve(listen)
+	if err != nil {
+		panic("failed to start grpc:" + err.Error())
+	}
 }
