112acf99ae781b394eda77cf57f40efe554fac9d
dezhizhang <1541609448@qq.com>
2023-09-17 15:17:46 +0800

    feat: 更新轮播图功能完善

--- handler/banner.go
+++ handler/banner.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"context"
+	"fmt"
 	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -52,12 +53,10 @@ func (b *BannerServer) GetBannerList(ctx context.Context, req *proto.BannerReque
 		return nil, status.Errorf(codes.Internal, "查询失败")
 	}
 
-	rsp := &proto.BannerResponseList{}
-
 	// 分页
 	global.DB.Scopes(utils.Paginate(int(req.PageIndex), int(req.PageSize))).Find(&banners)
 	//查询所有总数
-	global.DB.Model(&model.Brands{}).Count(&count)
+	global.DB.Model(&model.Banner{}).Count(&count)
 	for _, value := range banners {
 		bannerList = append(bannerList, &proto.BannerResponse{
 			Id:          value.Id,
@@ -69,10 +68,11 @@ func (b *BannerServer) GetBannerList(ctx context.Context, req *proto.BannerReque
 			Description: value.Description,
 		})
 	}
-	rsp.Total = int32(count)
-	rsp.Data = bannerList
 
-	return rsp, nil
+	fmt.Println("data", bannerList)
+	fmt.Println("Total", count)
+
+	return &proto.BannerResponseList{Data: bannerList, Total: int32(count)}, nil
 }
 
 // 删除轮播图
@@ -88,24 +88,28 @@ func (b *BannerServer) DeleteBanner(ctx context.Context, req *proto.DeleteBanner
 	return &empty.Empty{}, nil
 }
 
-// 更新轮播图
-
+// UpdateBanner 更新轮播图
 func (b *BannerServer) UpdateBanner(ctx context.Context, req *proto.UpdateBannerRequest) (*empty.Empty, error) {
-	var banner model.Banner
-	result := global.DB.Where("id = ?", req.Id).Find(&banner)
+	//var banner model.Banner
+	result := global.DB.Where("id = ?", req.Id).Find(&model.Banner{})
 	if result.RowsAffected == 0 {
 		return nil, status.Errorf(codes.Internal, "更新的轮播图不存在")
 	}
+
+	banner := &model.Banner{
+		Name:        req.Name,
+		Link:        req.Link,
+		Url:         req.Url,
+		Position:    req.Position,
+		Status:      req.Status,
+		Description: req.Description,
+	}
 	banner.Id = req.Id
-	banner.Url = req.Url
-	//banner.Image = req.Image
-	//banner.Index = req.Index
 	banner.UpdatedAt = time.Now()
 
-	err := global.DB.Save(&banner).Error
+	err := global.DB.Updates(&banner).Error
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "更新轮播图失败")
 	}
 	return &empty.Empty{}, nil
-
 }
